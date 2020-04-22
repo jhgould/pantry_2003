079eb6a246a203fcc7e6dcd7d4f47cfc8fd5299d
Jack Gould <johnhilsmangould@gmail.com>
2020-04-22 09:31:07 -0600

    add recipe methods and tests

---
+++ test/recipe_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/pantry'
+require './lib/ingredient'
+require './lib/recipe'
+
+class RecipeTest < Minitest::Test
+  def setup
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @recipe1 = Recipe.new("Mac and Cheese")
+  end
+
+  def test_it_exsits
+    assert_instance_of Recipe, @recipe1
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "Mac and Cheese", @recipe1.name
+    assert_equal ({}), @recipe1.ingredients_required
+  end
+
+  def test_it_can_add_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    hash = {@ingredient1 => 6, @ingredient2 => 8}
+    assert_equal hash, @recipe1.ingredients_required
+  end
+
+  def test_it_can_list_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    array = [@ingredient1, @ingredient2]
+    assert_equal array, @recipe1.ingredients
+  end
+
+end
